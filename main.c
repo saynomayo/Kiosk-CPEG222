21d4043f99bcb1ef01a7ff5dc9debc5632f8a81a
mekha <mekha@128.4.80.142>
2024-10-23 19:02:05 -0400

    pretty much done.

--- main.c
+++ main.c
@@ -24,6 +24,7 @@
 #include "lcd.h"    // Digilent Library for using the on-board LCD
 #include "acl.h"    // Digilent Library for using the on-board accelerometer
 #include "ssd.h" // Digilent Library for using the on-board SSD
+#include "led.h"
 
 #define TRUE 1
 #define FALSE 0
@@ -39,13 +40,27 @@
 #define C2 PORTDbits.RD10
 #define C1 PORTDbits.RD8
 
+#define RGSIN_SIZE  (sizeof(rgSinSamples) / sizeof(rgSinSamples[0]))
+#define TMR_FREQ_SINE   48000 // 48 kHz
+
 typedef enum _KEY {K0, K1, K2, K3, K4, K5, K6, K7, K8, K9, K_A, K_B, K_C, K_D, K_E, K_F, K_NONE} eKey ;
 typedef enum _MODE {MODE1,MODE2,MODE3,MODE4,MODE5,MODE6} eModes ;
+int seconds = 0;
 int digit1 = -1;
 int digit2 = -1;
 int digit3 = -1;
 int digit4 = -1;
 int ssd_index = 1;
+int JINGLE = 0;
+
+int cntAudioBuf, idxAudioBuf;
+
+unsigned short *pAudioSamples;
+
+unsigned short rgSinSamples [] = {200, 200, 200, 200, 200, 200, 200,
+                                  200, 200, 200, 200, 200, 200, 200,
+                                  200, 200, 200, 200, 200, 0, 0,
+                                  0, 0, 0};
 
 eModes mode = MODE4;
 
@@ -53,6 +68,8 @@ char new_press = FALSE;
 
 // subrountines
 void CNConfig();
+void TIMERConfig();
+void JINGLEConfig();
 void handle_new_keypad_press(eKey key) ;
 void mode1();
 void mode2();
@@ -60,6 +77,8 @@ void mode4();
 void mode1_input(eKey key);
 void mode2_input(eKey key);
 void mode4_input(eKey key);
+void TurnOnJingle(void);
+void TurnOffJingle(void);
 
 void initialize_ports();
 
@@ -92,6 +111,7 @@ int main(void) {
     
     //Initialize Visual Outputs
     SSD_Init();
+    LED_Init();
     
     // You need to enable all the rows
     R1 = R2 = R3 = R4 = 0;
@@ -101,11 +121,14 @@ int main(void) {
     LCD_WriteStringAtPos("Mode 4",1,4);
     
     CNConfig();
+    TIMERConfig();
+    JINGLEConfig();
+    
     SSD_WriteDigits(digit4, digit3, digit2, digit1, 0, 0, 0, 0);
 
     /* Other initialization and configuration code */
     while (TRUE) {
-        //SSD changed by keypad
+        
     }
 } 
 
@@ -126,6 +149,38 @@ void CNConfig() {
     
     int j = PORTD;             //read port to clear mismatch on CN pins
     macro_enable_interrupts();	// re-enable interrupts
+}
+
+void TIMERConfig() {
+    macro_disable_interrupts;
+    T2CONbits.ON = 0;
+    T2CONbits.TCKPS = 0b111;
+    T2CONbits.TCS = 0;
+    TMR2 = 0;
+    PR2 = (10000000 / 256);
+    IPC2bits.T2IP = 4;
+    IPC2bits.T2IS = 0;
+    IFS0bits.T2IF = 0;
+    IEC0bits.T2IE = 1;
+    T2CONbits.ON = 1;
+    macro_enable_interrupts();
+}
+
+void __ISR(_TIMER_2_VECTOR) TM_HANDLER(void) {
+    LATA = (0xFF00 | 0xFF >> (seconds%9));
+    seconds++;
+    TurnOffJingle();
+    IFS0bits.T2IF = 0;
+    macro_enable_interrupts();
+}
+
+void __ISR(_TIMER_3_VECTOR, IPL7AUTO) Timer3ISR(void) 
+{  
+    // play sine
+    // load sine value into OC register
+    OC1RS = 2*pAudioSamples[(++idxAudioBuf) % cntAudioBuf];
+    
+    IFS0bits.T3IF = 0;      // clear Timer3 interrupt flag
 }
 
 // This ISR is for the change notice interrupt. The timer will use another interrupt.
@@ -465,7 +520,12 @@ void mode4_input(eKey key){
             ssd_index--;
         }
         else if (key == K_E) {
-            play_jingle();
+            TurnOnJingle();
+            ssd_index = 1;
+            digit1 = -1;
+            digit2 = -1;
+            digit3 = -1;
+            digit4 = -1;
         }
         SSD_WriteDigits(digit4, digit3, digit2, digit1, 0, 0, 0, 0);
     }  
@@ -474,6 +534,48 @@ void mode4_input(eKey key){
     }
 }
 
-void play_jingle() {
+void JINGLEConfig(void) {
+    macro_disable_interrupts;
+     // the following lines configure interrupts to control the speaker
+    T3CONbits.ON = 0;       // turn off Timer3
+    OC1CONbits.ON = 0;      // Turn off OC1
+        /* The following code sets up the alarm timer and interrupts */
+    tris_A_OUT = 0;    
+    rp_A_OUT = 0x0C; // 1100 = OC1
+        // disable analog (set pins as digital)
+    ansel_A_OUT = 0;
     
+    T3CONbits.TCKPS = 0;     //1:1 prescale value
+    T3CONbits.TGATE = 0;     //not gated input (the default)
+    T3CONbits.TCS = 0;       //PCBLK input (the default)
+    
+    OC1CONbits.ON = 0;       // Turn off OC1 while doing setup.
+    OC1CONbits.OCM = 6;      // PWM mode on OC1; Fault pin is disabled
+    OC1CONbits.OCTSEL = 1;   // Timer3 is the clock source for this Output Compare module
+    
+    IPC3bits.T3IP = 7;      // interrupt priority
+    IPC3bits.T3IS = 3;      // interrupt subpriority
+    macro_enable_interrupts();
+}
+
+void TurnOnJingle(void) {
+    //set up alarm
+    PR3 = (int)((float)((float)PB_FRQ/TMR_FREQ_SINE) + 0.5);               
+    idxAudioBuf = 0;
+    cntAudioBuf = RGSIN_SIZE;
+    pAudioSamples = rgSinSamples;
+
+    // load first value
+    OC1RS = pAudioSamples[0];
+    TMR3 = 0;
+
+    T3CONbits.ON = 1;        //turn on Timer3
+    OC1CONbits.ON = 1;       // Start the OC1 module  
+    IEC0bits.T3IE = 1;      // enable Timer3 interrupt    
+    IFS0bits.T3IF = 0;      // clear Timer3 interrupt flag
+}
+
+void TurnOffJingle(void) {
+    T3CONbits.ON = 0;       // turn off Timer3
+    OC1CONbits.ON = 0;      // Turn off OC1
 }
